91b9338bb9115ab4112a2b1f6e0e66380ea320cc
timmens <tim@mensinger96.com>
2021-02-26 10:44:08 +0100

    first untested draft

---
+++ src/shared.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+
+from itertools import count
+from itertools import combinations
+
+
+def pick_best_candidate(candidates, grouping_df):
+    scores = np.empty(len(candidates))
+    for k, candidate in enumerate(candidates):
+        updated_grouping_df = update_grouping_df(grouping_df, candidate)
+        scores[k] = score_grouping_df(updated_grouping_df)
+
+    minimum = candidates[np.argmin(scores)]
+    updated_grouping_df = update_grouping_df(grouping_df, minimum)
+    return updated_grouping_df, minimum 
+
+
+def score_grouping_df(grouping_df, penalty_func=None):
+    """Score grouping matrix.
+
+    Scores grouping matrix such that having many large values is penalized.
+
+    Args:
+        grouping_df (pd.DataFrame): Square df containing group information. Index
+            and column is given by the "id" column in SRC.data.names.csv.
+        penalty_func (callable): Penalty function, defaults to np.exp. Is applied to
+            punish large values in grouping_df.
+    
+    Returns:
+        score (float): Score of grouping matrix.
+
+    """
+    penalty_func = np.exp if penalty_func is None else penalty_func
+
+    values = grouping_df.values
+    tril = np.tril(values).flatten()
+    
+    unique_values = np.unique(tril)
+    counts = np.bincount(tril)
+
+    score = np.sum(counts * penalty_func(unique_values))
+    return score
+
+
+def update_grouping_df(grouping_df, new_grouping_candidate):
+    """Update grouping matrix using new grouping candidate.
+
+    Args:
+        grouping_df (pd.DataFrame): Square df containing group information. Index
+            and column is given by the "id" column in SRC.data.names.csv.
+        new_grouping_candidate (list-like): Grouping candidate.
+
+    Returns:
+        updated_grouping_df (pd.DataFrame): Updated grouping matrix where group
+            membership is noted as +1 in the respective column-row of grouping matrix.
+
+    """
+    updated_grouping_df = grouping_df.copy()
+    
+    for group in new_grouping_candidate:
+        for comb in combinations(group, 2):
+            updated_grouping_df.loc[comb[0], comb[1]] += 1
+            updated_grouping_df.loc[comb[1], comb[0]] += 1  # So that matrix is symmetric
+
+    return updated_grouping_df
+
+
+def draw_groupings(members, min_size, n_candidates, initial_seed=0):
+    """Create multiple random groupings in list-format.
+
+    Args:
+        members (pd.Series): Integer series with indeces over which we group.
+        min_size (int): Minimum group size.
+        n_candidates (int): Number of candidate groups to simulate.
+        initial_seed (int): Initial seed to pass to the seed generator.
+
+    Returns:
+        candidates (list-like): List of candidate groupings.
+
+    """
+    seed_counter = count(initial_seed)
+    
+    candidates = []
+    for i in range(n_candidates):
+        grouping = create_grouping_candidate(members, min_size, next(seed_counter))
+        candidates.append(grouping)
+
+    return candidates
+
+
+def create_grouping_candidate(members, min_size, seed):
+    """Create single grouping candidate.
+
+    Args:
+        members (pd.Series): Integer series with indeces over which we group.
+        min_size (int): Minimum group size.
+        n_candidates (int): Number of candidate groups to simulate.
+        initial_seed (int): Initial seed to pass to the seed generator.
+
+    Returns:
+        candidate (list-like): Grouping candidate.
+
+    """
+    np.random.seed(seed)
+    members = members.values.copy()
+    np.random.shuffle(members)
+    candidate = create_chunks(members, min_size)
+    return candidate
+
+
+def create_chunks(members, min_size):
+    """Create chunks of subgroups of list of members.
+
+    Args:
+        members (np.ndarray): Integer array with indeces over which we group.
+        min_size (int): Minimum group size.
+
+    Returns:
+        chunks (list-like): Grouping candidate.
+
+    """
+    n_chunks = len(members) // min_size
+    chunks = np.array_split(members, n_chunks)
+    chunks = [set(chunk.tolist()) for chunk in chunks]
+    return chunks
--- src/task_create_grouping.py
+++ src/task_create_grouping.py
@@ -1,24 +1,88 @@
-"""Create grouping."""
+import os
+import yaml
+import pytask
+
 import numpy as np
 import pandas as pd
 
 from src.config import SRC
+from src.config import BLD
+from src.shared import draw_groupings
+from src.shared import score_grouping_df
+from src.shared import update_grouping_df
+from src.shared import pick_best_candidate
 
-
-def create_grouping_matrix():
-    path = SRC / "data" / "names.csv" 
+def read_names_df(path):
     names = pd.read_csv(path)
+    return names
+
+
+def read_config_kwargs(path):
+    with open(path) as f:
+        kwargs = yaml.load(f, Loader=yaml.FullLoader)
+    return kwargs
+
+
+def read_grouping_df(path, names):
+    if os.stat(path).st_size == 0:
+        grouping_df = create_grouping_df(names)
+    else:
+        grouping_df = pd.read_csv(path, index_col="id", header=0)
+        grouping_df.columns.name = "id"
+    return grouping_df
+
+
+def extract_current_members(names):
+    """Extract current members from names data frame.
+
+    Args:
+        names (pd.DataFrame): names.csv data file converted to pd.DataFrame
+    
+    Returns:
+        members (pd.Series): Series containing ids of individuals that participate.
+
+    """
+    members = names.query("joins == 1")["id"]
+    return members
+
+
+def create_grouping_df(names):
     id_ = names["id"]
     n = len(id_)
-    grouping = pd.DataFrame(np.zeros((n, n)), columns=id_, index=id_)
+    grouping = pd.DataFrame(np.zeros((n, n), dtype=int), columns=id_, index=id_)
     return grouping
 
 
+def update_grouping_df_file(updated_grouping_df, path):
+    updated_grouping_df.to_csv(path)
 
-def read_grouping_matrix():
-    path = SRC / "data" / "previous_groupings.csv"
-    if path.is_file():
-        grouping_matrix = pd.read_csv(path)
-    else:
-        grouping_matrix = create_grouping_matrix()
-    return grouping_matrix
+
+def write_pick(pick, names, path):
+    names = names.set_index("id")
+    texts = [", ".join(names["name"].loc[group].values) for group in pick]
+    
+    text = ""
+    for k, text_ in enumerate(texts):
+        text += f"Group {k}: {text_}\n"
+
+    with open(path, "w") as f:
+        f.write(text)
+
+
+@pytask.mark.depends_on(SRC / "config.yaml")
+@pytask.mark.depends_on(SRC / "data" / "names.csv")
+@pytask.mark.depends_on(SRC / "data" / "previous_groupings.csv")
+@pytask.mark.produces(BLD / "grouping.txt")
+def task_create_next_grouping(depends_on, produces):
+    kwargs = read_config_kwargs(depends_on[2])
+
+    names = read_names_df(depends_on[1])
+    members = extract_current_members(names)
+
+    grouping_df = read_grouping_df(depends_on[0], names)
+    candidates = draw_groupings(members, **kwargs)
+
+    updated_grouping_df, minimum = pick_best_candidate(candidates, grouping_df)
+
+    update_grouping_df_file(updated_grouping_df, depends_on[0])
+    write_pick(minimum, names, produces)
