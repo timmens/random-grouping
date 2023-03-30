1f5c7783f37e976bd88e79548742a7ae2f5272e5
timmens <mensingertim@gmail.com>
2023-03-30 18:39:37 +0200

    Update infrastructure

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name="randomgroups",
-    version="0.0.1",
-    author="Tim Mensinger",
-    author_email="tmensinger@uni-bonn.de",
-    url="https://github.com/timmens/random-grouping",
-    packages=find_packages(),
-    python_requires=">=3.6",
-    install_requires=["click", "numpy >=1.16", "pandas >=0.24", "openpyxl >= 3.0"],
-)
+
+if __name__ == "__main__":
+    setup()
